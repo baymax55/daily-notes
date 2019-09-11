f0c1617cf4fe3b651541afab562df9ba87873e5a
1348580915@qq.com <18161931993@163.com>
2019-09-11 14:15:58 +0800

    add test

---
+++ src/main/java/com/baymax/dailynotes/dailynotes/test/Test.java
@@ -0,0 +1,12 @@
+package com.baymax.dailynotes.dailynotes.test;
+
+/**
+ * @author bx
+ * @date 9/11/2019 9:32 AM
+ */
+public class Test {
+    public static void main(String[] args) {
+        //获取当前电脑的cpu数目
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
